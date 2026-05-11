6ad79dffb397e9a6570d33d34c57787ab43ef05e
Line Madsen <lima0001@stud.kea.dk>
2026-05-11 12:27:15 +0200

    ændring

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,8 @@ import YdelserCard from "./components/Ydelser-card";
 export default function Home() {
   return (
     <main>
-      <h2 className="text-center text-4xl mt-20 mb-20 text-gray-950">Hvad har du brug for hjælp til?</h2>
       <HeroForside />
-   
+      <h2 className="text-center text-4xl mt-20 mb-20 text-gray-950">Hvad har du brug for hjælp til?</h2>
       <YdelserCard />
       <Footer />
     </main>
